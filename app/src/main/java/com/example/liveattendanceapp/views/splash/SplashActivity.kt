967ae4377b2dfc34069c893676ffe3879944b8c6
Rida Angga Kusuma <rida.angga@gmail.com>
2023-01-20 20:08:45 +0700

    feat: check is login in SplashActivity

--- app/src/main/java/com/example/liveattendanceapp/views/splash/SplashActivity.kt
+++ app/src/main/java/com/example/liveattendanceapp/views/splash/SplashActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.liveattendanceapp.R
+import com.example.liveattendanceapp.hawkstorage.HawkStorage
 import com.example.liveattendanceapp.views.login.LoginActivity
+import com.example.liveattendanceapp.views.main.MainActivity
 import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -18,8 +20,21 @@ class SplashActivity : AppCompatActivity() {
 
     private fun afterDelayGoToLogin() {
         Handler(Looper.getMainLooper()).postDelayed({
+            checkIsLogin()
             startActivity<LoginActivity>()
             finishAffinity() //ketika masuk login activity, lalu klik back, maka akan destroy (tidak akan back ke splash, jadi tetap di halaman login)
         },1200) //delay 1,2 detik.
+    }
+
+    //Jika sudah login, masuk ke MainActivity, jika belum, masuk ke LoginActivity.
+    private fun checkIsLogin() {
+        val isLogin = HawkStorage.instance(this).isLogin()
+        if (isLogin){
+            startActivity<MainActivity>()
+            finishAffinity()
+        }else{
+            startActivity<LoginActivity>()
+            finishAffinity()
+        }
     }
 }
